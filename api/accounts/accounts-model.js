4ae4b595f22a44dbed16c595671864b19330e394
Astrospence <illustrator.wood@gmail.com>
2022-01-17 15:47:04 -0700

    fleshed out initial attempt accounts-model.js

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,27 @@
-const getAll = () => {
-  // DO YOUR MAGIC
+const db = require('../../data/db-config')
+
+ const getAll = async () => {
+  return db('accounts').select('id', 'name', 'budget')
 }
 
-const getById = id => {
-  // DO YOUR MAGIC
+const getById = async id => {
+  return db('accounts').where('id', id).first()
 }
 
-const create = account => {
-  // DO YOUR MAGIC
+const create = async account => {
+  const [newAccountId] = await db('accounts').insert(account)
+  return getById(newAccountId)
 }
 
-const updateById = (id, account) => {
-  // DO YOUR MAGIC
+const updateById = async (id, changes) => {
+  await db('accounts').where('id', id).update(changes)
+  return getById(id)
 }
 
-const deleteById = id => {
-  // DO YOUR MAGIC
+const deleteById = async id => {
+  const toBeDeleted = await getById(id)
+  await db('accounts').where('id', id).del()
+  return toBeDeleted
 }
 
 module.exports = {
